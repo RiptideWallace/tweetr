017750a6616f9eefeda2fb0d3a3192b07c9016a5
RiptideWallace <brysonc1987@gmail.com>
2017-07-11 22:54:15 +0000

    Tweeter Project up to Fetching Tweets with AJAX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -54,32 +54,41 @@ var data = [
 
 jQuery(document).ready(function() {
 
-function createTweetElement(tweet) {
-  var $tweet = $('<article>').addClass('tweet')
-    .append($('<header class="tweet-header">')
-      .append($('<img class="avatar">').attr("src", tweet.user.avatars.small))
-      .append($('<h2 class="name">').text(tweet.user.name))
-      .append($('<p class="username">').text(tweet.user.handle))
-    )
+  function createTweetElement(tweet) {
+    var $tweet = $('<article>').addClass('tweet')
+      .append($('<header class="tweet-header">')
+        .append($('<img class="avatar">').attr("src", tweet.user.avatars.small))
+        .append($('<h2 class="name">').text(tweet.user.name))
+        .append($('<p class="username">').text(tweet.user.handle))
+      )
 
-    .append($('<p class="tweet-content">').text(tweet.content.text))
+      .append($('<p class="tweet-content">').text(tweet.content.text))
 
-    .append($('<footer class="tweet-footer">')
-      .append($('<p class="time-stamp">').text(tweet.created_at))
-      .append($('<input class="flag" type="image" alt="submit">').attr("src", "/images/flag.png"))
-      .append($('<input class="RT" type="image" alt="submit">').attr("src", "/images/rt.png"))
-      .append($('<input class="heart" type="image" alt="submit" >').attr("src", "/images/heart.png"))
-    )
-  return $tweet;
-}
+      .append($('<footer class="tweet-footer">')
+        .append($('<p class="time-stamp">').text(tweet.created_at))
+        .append($('<input class="flag" type="image" alt="submit">').attr("src", "/images/flag.png"))
+        .append($('<input class="RT" type="image" alt="submit">').attr("src", "/images/rt.png"))
+        .append($('<input class="heart" type="image" alt="submit" >').attr("src", "/images/heart.png"))
+      )
+    return $tweet;
+  }
 
-function renderTweets(tweets) {
-  tweets.forEach(function(tweet){
-    $(".container").append(createTweetElement(tweet));
-  });
-}
-  renderTweets(data);
+  function renderTweets(tweets) {
+    tweets.forEach(function(tweet){
+      $(".container").append(createTweetElement(tweet));
+    });
+  }
+    renderTweets(data);
 
+  function loadTweets() {
+    $.ajax({
+      url: 'http://localhost:8080/tweets',
+      method: 'GET',
+      success: function(renderTweets) {
+        console.log('Success:', initialTweets);
+      }
+    });
+  }
 });
 
 
