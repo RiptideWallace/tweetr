11c94e75acb9051c78468c9b12e894c2d26cbbcd
RiptideWallace <brysonc1987@gmail.com>
2017-07-12 01:24:09 +0000

    Tweeter Project up to Form Validation Complete

--- public/scripts/formsubmission.js
+++ public/scripts/formsubmission.js
@@ -1,16 +1,17 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function(){
+  $('.tweet-button').on('click', function(){
     let maxLength = 140;
     let length = $('textarea').val().length;
-  $('.tweet-button').on('click' function(){
-      if (length === null) {
-        alert("Please Input Text to Submit");
-        return false;
-      } else if (length > maxlength) {
-        alert("Your Tweet Is Too Long");
-        return false;
-      } else {
-        return true;
-      }
+    if(length == 0) {
+      alert("Please Input Text to Submit");
+      return false;
+    } else if (length > maxLength) {
+      alert("Your Tweet is Too Long");
+      return false;
+    } else {
+      return true;
+    }
   });
 });
+
 
