404dad63f0caf00946bffda7c78968fd4f2db388
RiptideWallace <brysonc1987@gmail.com>
2017-07-13 21:32:33 +0000

    project ready to submit

--- server/index.js
+++ server/index.js
@@ -17,46 +17,6 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   const DataHelpers = require("./lib/data-helpers.js")(db);
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
-  //Registration Page
-  app.get("/register", (req, res) => {
-
-    res.sendfile("./public/register.html")
-  });
-
-  //Action when a person registers
-  app.post("/register", (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
-    let name = req.body.name;
-    let handle = req.body.handle;
-    let avatar = req.body.avatar;
-
-    if (email == false || password == false || name == false ||
-        handle == false || avatar == false) {
-        res.status(404).send('Form Data Missing');
-        return;
-    }
-      res.redirect("/");
-   });
-
-  //Login Page
-  app.get("/login", (req, res) => {
-
-    res.sendfile("./public/login.html")
-  });
-
-  //Action when a user logs-in
-  app.post("/login", (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
-
-    if (email == false || password == false) {
-      res.status(404).send('Form Data Missing');
-      return;
-    }
-    res.redirect("/");
-  });
-
   app.use("/tweets", tweetsRoutes);
 
   app.listen(PORT, () => {
