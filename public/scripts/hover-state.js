75520e7fcf28720dca1152e4479fa761f05a7472
ChrisBryson <brysonc1987@gmail.com>
2017-07-13 17:42:35 -0700

    Delete hover-state.js

--- public/scripts/hover-state.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-  $(".tweet-footer").find(":image").hide();
-    $('.tweet-footer').mouseenter(function() {
-      $(this).find(":image").show()
-  }).on('mouseleave', this, function () {
-      $(this).find(":image").hide();
-  });
-});
